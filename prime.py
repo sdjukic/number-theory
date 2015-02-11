6e1b4596edcad5e871393053a9d2232ff63cfb2a
Slavisa Djukic <Slavisa.Djukic15@myhunter.cuny.edu>
2015-02-11 05:50:11 -0500

    Two sieving algos.

---
+++ prime.py
@@ -0,0 +1,47 @@
+# Library for dealing with prime numbers
+from math import sqrt
+
+def isprime(number):
+    if number % 2 == 0 or number % 3 == 0: return False
+    for divisor in range(5, int(sqrt(number)+1), 2):
+        if number % divisor == 0: return False
+
+    return True
+
+# intuitive implementation of sieve
+def primes_sieve(limit):
+    a = [True] * limit
+    a[0] = a[1] = False
+
+    for (i, isprime) in enumerate(a):
+        if isprime:
+            yield i
+            for n in range(i*i, limit, i): #Mark factors non-prime
+                a[n] = False
+
+
+# this is from active state
+def postponed_sieve():
+    yield 2; yield 3; yield 5; yield 7;
+    sieve = {}
+    ps = (p for p in postponed_sieve())    # a separate Primes supply
+    p = next(ps) and next(ps)              # (3) a Prime to add to dict
+    q = p*p                                # (9) when its square
+    c = 9                                  # is the next candidate
+    
+    while True:
+        if c not in sieve:                 # not a multiple of any prime seen so far
+            if c < q:                      # a prime
+                yield c; c += 2;
+                continue
+            else:   # (c==q)               # the next prime's square:
+                s = 2*p                    # (9+6, 6 : 15, 21, 27, 33,...)
+                p = next(ps)               # (5)
+                q = p*p                    # (25)
+        else:                              # 'c' is a composite
+            s = sieve.pop(c)               # step of increment
+        c2 = c + s                         # next multiple, same step
+        while c2 in sieve:                 # no multiple key in sieve (dict):
+            c2 += s                        #  (increments by the given step)
+        sieve[c2] = s                   
+        c += 2                             # next odd candidate
